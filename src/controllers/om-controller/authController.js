d6cfabc99e6b4fba20a79043ac2e88ac4be5c058
Dicki Prasetya <dickiprasetya176@gmail.com>
2024-05-12 20:18:48 +0700

    feat: add branch om dev

---
+++ src/controllers/om-controller/authController.js
@@ -0,0 +1,113 @@
+import bcrypt from "bcrypt";
+import { UserModel, userSchema } from "../../models/userModel.js";
+import { apiResponseList, apiResponse } from "../../utils/apiResponseFormat.js";
+import { generateToken } from "../../utils/jwt.js";
+import { StoreModel, storeSchema } from "../../models/storeModel.js";
+import {
+  connectTargetDatabase,
+  closeConnection,
+} from "../../config/targetDatabase.js";
+
+//NOT USE
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return apiResponse(res, 400, "Incorrect email or password");
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return apiResponse(res, 400, "Incorrect email or password.");
+    }
+    return apiResponse(res, 200, "Login successful", user);
+  } catch (error) {
+    console.error("Error:", error);
+    return apiResponse(res, 500, "Internal Server Error");
+  }
+};
+
+const signinWithGoogle = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const user = await UserModel.findOne({ email, isRaku: true });
+
+    if (!user) {
+      const newUser = await UserModel({ email, isRaku: true });
+      const dataUser = await newUser.save();
+      const token = generateToken({ data: dataUser._id });
+      newUser.token = token;
+      newUser.store_database_name.sort(
+        (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+      );
+
+      const result = [];
+
+      for (const db of newUser.store_database_name) {
+        console.log(db.name);
+        const name = db.name;
+        const database = await connectTargetDatabase(db.name);
+        const StoreModelDatabase = database.model("Store", storeSchema);
+        const data = await StoreModelDatabase.findOne();
+        if (data != null) {
+          result.push({
+            user: newUser,
+            dbName: name,
+            storesData: data,
+            email_owner: email,
+            token: token,
+          });
+          console.log(result);
+        }
+      }
+      console.log({ user: newUser, result: result });
+      return apiResponse(res, 200, "Akun berhasil didaftarkan", {
+        user: newUser,
+        database: result,
+      });
+    }
+    user.store_database_name.sort(
+      (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+    );
+
+    const token = generateToken({ data: user._id });
+    user.token = token;
+
+    const result = [];
+
+    for (const db of user.store_database_name) {
+      console.log(db.name);
+      const name = db.name;
+      const database = await connectTargetDatabase(db.name);
+      const StoreModelDatabase = database.model("Store", storeSchema);
+      const data = await StoreModelDatabase.findOne();
+      // if (data != null) {
+      result.push({
+        user: user,
+        dbName: name,
+        storesData: data,
+        email_owner: email,
+        token: token,
+      });
+      console.log(result);
+      // }
+    }
+    console.log({ user: user, result: result });
+    return apiResponse(res, 200, "Akun ditemukan", {
+      user: user,
+      database: result,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    return apiResponse(res, 500, "Internal Server Error");
+  }
+};
+
+const logout = (req, res) => {
+  res.json({ message: "Logout successful" });
+};
+
+export default { login, logout, signinWithGoogle };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,164 +1,231 @@
 // routes/storeRoutes.js
-import express from 'express';
-import storeController from '../controllers/storeController.js';
-import authController from '../controllers/authController.js';
-import brandControler from '../controllers/brandController.js';
-import categoryController from '../controllers/categoryController.js';
-import productController from '../controllers/productController.js';
-import unitController from '../controllers/unitController.js';
-import cartController from '../controllers/cartController.js';
-import paymentController from '../controllers/paymentController.js';
-import splitPaymentRuleController from '../controllers/splitPaymentRuleController.js';
-import historyTransactionController from '../controllers/historyTransactionController.js';
-import merchantController from '../controllers/merchantController.js';
-import configController from '../controllers/configController.js';
-import withdrawlController from '../controllers/withdrawlController.js';
-import { body } from 'express-validator';
-import { verifyToken } from '../utils/jwt.js'; 
-import { verifyXenditToken } from '../utils/xenditToken.js';
+import express from "express";
+import storeController from "../controllers/storeController.js";
+import authController from "../controllers/authController.js";
+import brandControler from "../controllers/brandController.js";
+import categoryController from "../controllers/categoryController.js";
+import productController from "../controllers/productController.js";
+import unitController from "../controllers/unitController.js";
+import cartController from "../controllers/cartController.js";
+import paymentController from "../controllers/paymentController.js";
+import splitPaymentRuleController from "../controllers/splitPaymentRuleController.js";
+import historyTransactionController from "../controllers/historyTransactionController.js";
+import merchantController from "../controllers/merchantController.js";
+import configController from "../controllers/configController.js";
+import withdrawlController from "../controllers/withdrawlController.js";
+import { body } from "express-validator";
+import { verifyToken } from "../utils/jwt.js";
+import { verifyXenditToken } from "../utils/xenditToken.js";
 
+//raku
+import authControllerRaku from "../controllers/om-controller/authController.js";
+import storeControllerRaku from "../controllers/om-controller/storeController.js";
 
 const router = express.Router();
 
+//authenticate raku
+router.post(
+  "/raku/auth/signin_with_google",
+  authControllerRaku.signinWithGoogle
+);
+
 //authenticate
-router.get('/config/version', configController.versionApps);
-router.get('/config/version/v2', configController.versionAppsV2);
-router.post('/auth/login', authController.login);
-router.post('/auth/signin_with_google', authController.signinWithGoogle);
-router.post('/auth/logout', authController.logout);
+router.get("/config/version", configController.versionApps);
+router.get("/config/version/v2", configController.versionAppsV2);
+router.post("/auth/login", authController.login);
+router.post("/auth/signin_with_google", authController.signinWithGoogle);
+router.post("/auth/logout", authController.logout);
 
 //approval merchant belum dilindungi
-router.post('/store/merchant/approval', merchantController.approvalRequestMerchant);
-
+router.post(
+  "/store/merchant/approval",
+  merchantController.approvalRequestMerchant
+);
 
 //WEBHOOK
 
 // WEBHOOK QRIS;l
-router.post('/webhook/:db', paymentController.xenditWebhook);
+router.post("/webhook/:db", paymentController.xenditWebhook);
 // WEBHOOK VA
-router.use('/webhook_va/:type', verifyXenditToken);
-router.post('/webhook_va/:type', paymentController.webhookVirtualAccount);
+router.use("/webhook_va/:type", verifyXenditToken);
+router.post("/webhook_va/:type", paymentController.webhookVirtualAccount);
 // WEBHOOK WITHDRAW
-router.use('/webhook_withdraw', verifyXenditToken);
-router.post('/webhook_withdraw', withdrawlController.webhookWithdraw);
+router.use("/webhook_withdraw", verifyXenditToken);
+router.post("/webhook_withdraw", withdrawlController.webhookWithdraw);
 
+router.use(verifyToken);
 
+// store raku
+router.post("/raku/store/register", storeControllerRaku.registerStore);
 
-
-router.use(verifyToken); 
 // store
-router.post('/store/register', storeController.registerStore);
-router.post('/store/register_cashier', storeController.registerCashier);
-router.post('/store/remove_cashier', storeController.removeCashier);
-router.post('/store/update', storeController.updateStore);
-router.post('/store/update/add_bank_account', storeController.addBankAccount);
-router.post('/store/update/request_bussiness_partner', storeController.requestBussinessPartner);
-router.get('/store/get-info', storeController.getStoreInfo);
-router.get('/store/get_stores_database', storeController.getAllStoreInDatabase);
-router.get('/store/get_stores_database_id_parent', storeController.getStoresByParentId);
-router.get('/store/get_stores_database_id_parent/trx', storeController.getTrxNotRegisteredInTemplateByIdParent);
-
+router.post("/store/register", storeController.registerStore);
+router.post("/store/register_cashier", storeController.registerCashier);
+router.post("/store/remove_cashier", storeController.removeCashier);
+router.post("/store/update", storeController.updateStore);
+router.post("/store/update/add_bank_account", storeController.addBankAccount);
+router.post(
+  "/store/update/request_bussiness_partner",
+  storeController.requestBussinessPartner
+);
+router.get("/store/get-info", storeController.getStoreInfo);
+router.get("/store/get_stores_database", storeController.getAllStoreInDatabase);
+router.get(
+  "/store/get_stores_database_id_parent",
+  storeController.getStoresByParentId
+);
+router.get(
+  "/store/get_stores_database_id_parent/trx",
+  storeController.getTrxNotRegisteredInTemplateByIdParent
+);
 
 //brand
-router.get('/store/brand', brandControler.getAllBrands);
-router.post('/store/brand/create', brandControler.createBrand);
-router.post('/store/brand/edit', brandControler.editBrand);
-router.post('/store/brand/delete/:id', brandControler.deleteBrand);
-router.get('/store/brand/:id', brandControler.getBrandById);
+router.get("/store/brand", brandControler.getAllBrands);
+router.post("/store/brand/create", brandControler.createBrand);
+router.post("/store/brand/edit", brandControler.editBrand);
+router.post("/store/brand/delete/:id", brandControler.deleteBrand);
+router.get("/store/brand/:id", brandControler.getBrandById);
 
 //category
-router.post('/store/category/create', categoryController.createCategory);
-router.post('/store/category/edit', categoryController.editCategory);
-router.get('/store/category', categoryController.getAllCategories);
-router.post('/store/category/delete/:id', categoryController.deleteCategory);
-router.get('/store/category/:id', categoryController.getSingleCategories);
-
-
+router.post("/store/category/create", categoryController.createCategory);
+router.post("/store/category/edit", categoryController.editCategory);
+router.get("/store/category", categoryController.getAllCategories);
+router.post("/store/category/delete/:id", categoryController.deleteCategory);
+router.get("/store/category/:id", categoryController.getSingleCategories);
 
 //product
-router.post('/store/product/create', productController.createProduct);
-router.post('/store/product/edit', productController.editProduct);
-router.post('/store/product/delete/:id', productController.deleteProduct);
-router.get('/store/product', productController.getAllProducts);
-router.get('/store/product/icon', productController.getIconProducts);
-router.get('/store/product/:id', productController.getSingleProduct);
+router.post("/store/product/create", productController.createProduct);
+router.post("/store/product/edit", productController.editProduct);
+router.post("/store/product/delete/:id", productController.deleteProduct);
+router.get("/store/product", productController.getAllProducts);
+router.get("/store/product/icon", productController.getIconProducts);
+router.get("/store/product/:id", productController.getSingleProduct);
 
 //unit
-router.post('/store/unit/create', unitController.createUnit);
-router.post('/store/unit/edit', unitController.editUnit);
-router.get('/store/unit', unitController.getAllUnits);
-router.post('/store/unit/delete/:id', unitController.deleteUnit);
-router.get('/store/unit/:id', unitController.getSingleUnits);
-
-
+router.post("/store/unit/create", unitController.createUnit);
+router.post("/store/unit/edit", unitController.editUnit);
+router.get("/store/unit", unitController.getAllUnits);
+router.post("/store/unit/delete/:id", unitController.deleteUnit);
+router.get("/store/unit/:id", unitController.getSingleUnits);
 
 //cart
-router.post('/store/cart/create', cartController.addToCart);
-router.post('/store/cart/clear_all', cartController.clearCart);
-router.get('/store/cart/:id', cartController.getCartByUser);
+router.post("/store/cart/create", cartController.addToCart);
+router.post("/store/cart/clear_all", cartController.clearCart);
+router.get("/store/cart/:id", cartController.getCartByUser);
 
 //payment
-router.get('/store/transcation/payment_available', paymentController.paymentAvailable);
-router.post('/store/transcation/create-invoices', paymentController.createInvoice);
-router.get('/store/transcation/invoces/:id', paymentController.getInvoices);
-router.get('/store/transcation/invoces/cancel/:id', paymentController.cancelInvoices);
-router.post('/store/transcation/create-qrcode', paymentController.createQrCode);
-router.post('/store/transcation/create-va', paymentController.createVirtualAccount);
-router.get('/store/transcation/qrcode/:id', paymentController.getQrCode);
-router.post('/store/transcation/payment-cash', paymentController.paymentCash);
+router.get(
+  "/store/transcation/payment_available",
+  paymentController.paymentAvailable
+);
+router.post(
+  "/store/transcation/create-invoices",
+  paymentController.createInvoice
+);
+router.get("/store/transcation/invoces/:id", paymentController.getInvoices);
+router.get(
+  "/store/transcation/invoces/cancel/:id",
+  paymentController.cancelInvoices
+);
+router.post("/store/transcation/create-qrcode", paymentController.createQrCode);
+router.post(
+  "/store/transcation/create-va",
+  paymentController.createVirtualAccount
+);
+router.get("/store/transcation/qrcode/:id", paymentController.getQrCode);
+router.post("/store/transcation/payment-cash", paymentController.paymentCash);
 
 //split payment rules
-router.post('/store/split_rule', splitPaymentRuleController.createSplitRule);
-router.get('/store/split_rule/:id', splitPaymentRuleController.getSplitRuleTRX);
-router.post('/store/crate_template', splitPaymentRuleController.createTemplate);
-router.post('/store/template/update', splitPaymentRuleController.updateTemplate);
-router.get('/store/split/:id', splitPaymentRuleController.getTemplateById);
-router.get('/store/template/all', splitPaymentRuleController.getAllTemplates);
-router.post('/store/template/change_status', splitPaymentRuleController.activationTemplate);
-router.post('/store/template/target/delete', splitPaymentRuleController.deleteTargetTemplate);
+router.post("/store/split_rule", splitPaymentRuleController.createSplitRule);
+router.get("/store/split_rule/:id", splitPaymentRuleController.getSplitRuleTRX);
+router.post("/store/crate_template", splitPaymentRuleController.createTemplate);
+router.post(
+  "/store/template/update",
+  splitPaymentRuleController.updateTemplate
+);
+router.get("/store/split/:id", splitPaymentRuleController.getTemplateById);
+router.get("/store/template/all", splitPaymentRuleController.getAllTemplates);
+router.post(
+  "/store/template/change_status",
+  splitPaymentRuleController.activationTemplate
+);
+router.post(
+  "/store/template/target/delete",
+  splitPaymentRuleController.deleteTargetTemplate
+);
 
 //merchanr
-router.post('/store/merchant/create', merchantController.createMerchant);
-router.get('/store/merchant/all', merchantController.getAllMerchant);
+router.post("/store/merchant/create", merchantController.createMerchant);
+router.get("/store/merchant/all", merchantController.getAllMerchant);
 // router.post('/store/merchant/approval', merchantController.approvalRequestMerchant);
-router.post('/store/merchant/accept_invitation', merchantController.acceptInvitationMerchant);
+router.post(
+  "/store/merchant/accept_invitation",
+  merchantController.acceptInvitationMerchant
+);
 
 //history transaction
-router.post('/store/transaction/history', historyTransactionController.historyTransaction);
-router.post('/store/transaction/history/report', historyTransactionController.historyTransactionReport);
-router.post('/store/transaction/get_total_income', historyTransactionController.getTotalIncomeTransaction);
-router.post('/store/transaction/get_total_bagi', historyTransactionController.getTotalIncomeBagi);
-router.post('/store/transaction/history/v2', historyTransactionController.historyTransactionV2);
-router.post('/store/transaction/history/product', historyTransactionController.transactionDetailNonSplit);
-router.post('/store/transaction/history/support', historyTransactionController.historyTransactionSupport);
-router.post('/store/transaction/history/detail', historyTransactionController.transactionDetail);
-router.post('/store/transaction/history/filter/:role', historyTransactionController.getFilterStore);
-router.post('/store/transaction/history/transaction/today', historyTransactionController.historyTransactionToday);
-
+router.post(
+  "/store/transaction/history",
+  historyTransactionController.historyTransaction
+);
+router.post(
+  "/store/transaction/history/report",
+  historyTransactionController.historyTransactionReport
+);
+router.post(
+  "/store/transaction/get_total_income",
+  historyTransactionController.getTotalIncomeTransaction
+);
+router.post(
+  "/store/transaction/get_total_bagi",
+  historyTransactionController.getTotalIncomeBagi
+);
+router.post(
+  "/store/transaction/history/v2",
+  historyTransactionController.historyTransactionV2
+);
+router.post(
+  "/store/transaction/history/product",
+  historyTransactionController.transactionDetailNonSplit
+);
+router.post(
+  "/store/transaction/history/support",
+  historyTransactionController.historyTransactionSupport
+);
+router.post(
+  "/store/transaction/history/detail",
+  historyTransactionController.transactionDetail
+);
+router.post(
+  "/store/transaction/history/filter/:role",
+  historyTransactionController.getFilterStore
+);
+router.post(
+  "/store/transaction/history/transaction/today",
+  historyTransactionController.historyTransactionToday
+);
 
 // //withdrawl
-router.get('/store/balance/get_balance', withdrawlController.getBalance);
-router.post('/store/balance/verify_pin', withdrawlController.verifyPinWIthdrawl);
-router.post('/store/balance/withdraw', withdrawlController.withdrawl);
-router.get('/store/balance/withdraw/history', withdrawlController.getWithdrawHistory);
-router.post('/store/balance/withdraw/check_amount', withdrawlController.withdrawCheckAmount);
-
-
-
+router.get("/store/balance/get_balance", withdrawlController.getBalance);
+router.post(
+  "/store/balance/verify_pin",
+  withdrawlController.verifyPinWIthdrawl
+);
+router.post("/store/balance/withdraw", withdrawlController.withdrawl);
+router.get(
+  "/store/balance/withdraw/history",
+  withdrawlController.getWithdrawHistory
+);
+router.post(
+  "/store/balance/withdraw/check_amount",
+  withdrawlController.withdrawCheckAmount
+);
 
 // test
-router.post('/test/garapin_cost', paymentController.testGarapinCost);
-router.get('/test/login', configController.loginTest);
-
+router.post("/test/garapin_cost", paymentController.testGarapinCost);
+router.get("/test/login", configController.loginTest);
 
 // router.post('/store/transcation/ewallet',paymentController.createEwallet);
 // router.post('/webhook/:id/:db' ,paymentController.xenditWebhook);
-
-
-
-
-
-
-
 
 export default router;
