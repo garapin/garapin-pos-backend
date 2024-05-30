17e950c59684d9e0dea7b9ae9f6903ec5af2f851
Dicki Prasetya <dickiprasetya176@gmail.com>
2024-05-30 10:06:11 +0700

    feat: update

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use("/uploads", express.static("uploads"));
 app.use("/images", express.static("images"));
 app.use("/assets", express.static("assets"));
 app.use("", routes);
-app.use("", rakuRouter);
+// app.use("", rakuRouter);
 
 app.use(notFound);
 // app.use(errorHandler);
