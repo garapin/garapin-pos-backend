04f73fbd3ff1b5068f276291521f0284b0c9ec7d
Dicki Prasetya <dickiprasetya176@gmail.com>
2024-06-07 00:26:31 +0700

    feat: fix send otp email

--- src/schema/emailSchema.js
+++ src/schema/emailSchema.js
@@ -1,8 +1,9 @@
 import { z } from "zod";
 
 const sendOtpVerificationSchema = z.object({
+  store_name: z.string(),
   email: z.string().email(),
-  about_what: z.string(),
+  about_what: z.string().optional(),
 });
 
 export { sendOtpVerificationSchema };
