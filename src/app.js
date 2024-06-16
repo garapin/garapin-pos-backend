1c394d5f07cd49030e5fc349612be95649ba5422
prima jatnika <primajatnika271995@gmail.com>
2024-06-16 09:42:47 +0700

    FEAT(engine-split): Create new engine for split or transfer payment from XENDIT ACCOUNT

--- src/app.js
+++ src/app.js
@@ -2,18 +2,20 @@ import express from 'express';
 import routes from './routes/routes.js';
 import 'dotenv/config';
 import { join, dirname } from 'path';
+import cron from 'node-cron';
 import { fileURLToPath } from 'url';
 import bodyParser from 'body-parser';
+import TransactionEngine from './controllers/transactionEngineController.js';
 
 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
 const app = express();
 const port = process.env.PORT || 4000;
 const host = process.env.HOST || 'localhost'; 
+const transactionEngine = new TransactionEngine();
 
 
 // Atur batas ukuran entitas menjadi 10MB
@@ -27,6 +29,12 @@ app.use('/uploads', express.static('uploads'));
 app.use('/images', express.static('images'));
 app.use('/assets', express.static('assets'));
 app.use('', routes);
+
+// Menjadwalkan cron job untuk berjalan setiap jam
+cron.schedule('*/10 * * * * *', () => {
+  console.log('Menjalankan cron job setiap jam');
+  transactionEngine.getXenditTransaction();
+});
 
 
 app.listen(port, host, () => {
--- src/controllers/splitPaymentRuleController.js
+++ src/controllers/splitPaymentRuleController.js
@@ -12,126 +12,126 @@ import moment from 'moment';
 
 
 const createSplitRule = async (req, res) => {
-    try {
-      const targetDatabase = req.get('target-database');
-      const apiKey = XENDIT_API_KEY; 
+  try {
+    const targetDatabase = req.get('target-database');
+    const apiKey = XENDIT_API_KEY;
 
-      if (!targetDatabase) {
-        return apiResponse(res, 400, 'Target database is not specified');
-      }
-    
-      
-      const { id_template, name, description, routes } = req.body;
-      if (!id_template) {
-        return apiResponse(res, 400, 'Template tidak ditemukan');
-      }
-      const totalPercentAmount = routes.reduce((acc, route) => acc + route.percent_amount, 0);
+    if (!targetDatabase) {
+      return apiResponse(res, 400, 'Target database is not specified');
+    }
+
+
+    const { id_template, name, description, routes } = req.body;
+    if (!id_template) {
+      return apiResponse(res, 400, 'Template tidak ditemukan');
+    }
+    const totalPercentAmount = routes.reduce((acc, route) => acc + route.percent_amount, 0);
     if (totalPercentAmount !== 100) {
-       return apiResponse(res, 400, 'total persen bagi-bagi pendapatan harus 100%');
+      return apiResponse(res, 400, 'total persen bagi-bagi pendapatan harus 100%');
     }
     const totalPercentFeePos = routes.reduce((acc, route) => acc + route.fee_pos, 0);
     if (totalPercentFeePos !== 100) {
-       return apiResponse(res, 400, 'total persen bagi-bagi biaya harus 100%');
+      return apiResponse(res, 400, 'total persen bagi-bagi biaya harus 100%');
     }
-        //template
-        const dbTemplate = await connectTargetDatabase(targetDatabase);
-        const TemplateModel = dbTemplate.model('Split_Payment_Rule', splitPaymentRuleIdScheme);
-        const template = await TemplateModel.findById(id_template);
-        if (!template) {
-          return apiResponse(res, 404, 'Template tidak ditemukan');
-        }
-        // end template 
-    
+    //template
+    const dbTemplate = await connectTargetDatabase(targetDatabase);
+    const TemplateModel = dbTemplate.model('Split_Payment_Rule', splitPaymentRuleIdScheme);
+    const template = await TemplateModel.findById(id_template);
+    if (!template) {
+      return apiResponse(res, 404, 'Template tidak ditemukan');
+    }
+    // end template 
 
-      const data = {
-        'name': name,
-        'description': description,
-        'routes': [],
-      };
+
+    const data = {
+      'name': name,
+      'description': description,
+      'routes': [],
+    };
     // Validasi dan pemetaan routes
-    const  garapinCost = 3;
-  const routesValidate = routes.map(route => {
-     return {
-      'percent_amount': route.percent_amount - (garapinCost/100 * route.fee_pos),
-      'currency': route.currency,
-      'destination_account_id': route.destination_account_id,
-      'reference_id': route.reference_id
-    }; 
-});
-console.log(routes);
-
-data.routes = routesValidate;
-data.routes.push({
-  'percent_amount': garapinCost,
-  'currency': 'IDR',
-  'destination_account_id': '6602d442a5e108f758e0651d',
-  'reference_id': 'garapin_pos'
-}); 
-
-console.log('total percent');
-console.log(totalPercentAmount);
-//check trx sudah terdaftar belum, karna satu trx satu split rule,
-const referenceIdTRX = routes.filter(item => item.type === 'TRX').map(item => item.reference_id);
-console.log(referenceIdTRX);
-const splitPaymentRuleId = await connectTargetDatabase(referenceIdTRX);
-const SplitPaymentRuleIdStore = splitPaymentRuleId.model('Split_Payment_Rule_Id', splitPaymentRuleIdScheme);
-const existRuleTRX = await SplitPaymentRuleIdStore.findOne({ id_template:id_template });
-if (!existRuleTRX) {
-  return apiResponse(res, 400, 'Sudah terdaftar ditemplate lain');
-}
-      const endpoint = 'https://api.xendit.co/split_rules';
-      const headers = {
-        'Authorization': `Basic ${Buffer.from(apiKey + ':').toString('base64')}`,
+    const garapinCost = 3;
+    const routesValidate = routes.map(route => {
+      return {
+        'percent_amount': route.percent_amount - (garapinCost / 100 * route.fee_pos),
+        'currency': route.currency,
+        'destination_account_id': route.destination_account_id,
+        'reference_id': route.reference_id
       };
-      const response = await axios.post(endpoint, data, { headers });
-      if (response.status === 200) {
-        for (const route of response.data.routes) {
-          const splitPaymentRuleId = await connectTargetDatabase(route.reference_id);
-          const SplitPaymentRuleIdStore = splitPaymentRuleId.model('Split_Payment_Rule_Id', splitPaymentRuleIdScheme);
-          
-           // search SplitRuleId sudah ada belum
-           const splitRuleId = await SplitPaymentRuleIdStore.findOne({ id_template:id_template });
-           if (!splitRuleId) {
-            const create = new SplitPaymentRuleIdStore({
-              id: response.data.id,
-              name: response.data.name,
-              description: response.data.description,
-              created_at: response.data.created_at,
-              updated_at: response.data.updated_at,
-              id_template: id_template, // Isi dengan nilai id_template yang sesuai
-              routes: response.data.routes
-            });
-            const data = await create.save();
-           } else {
-            const update = {
-              id: response.data.id,
-              name: response.data.name,
-              description: response.data.description,
-              created_at: response.data.created_at,
-              updated_at: response.data.updated_at,
-              routes: response.data.routes
-            };
-          const data = await SplitPaymentRuleIdStore.findOneAndUpdate(
-              { id_template: id_template },
-              update,
-              { new: true } 
-            );
-           }
-           // end search SplitRuleId sudah ada belum
-        }
-       
-        console.log(response.data);
-       
-         return apiResponse(res, 200, 'Sukses membuat split rule', response.data);
-      }
-      return apiResponse(res, 400, 'gagal');
-     
-    
-    } catch (error) {
-      console.error('Error:', error.response?.data || error.message);
-      return apiResponse(res, 400, 'error');
+    });
+    console.log(routes);
+
+    data.routes = routesValidate;
+    data.routes.push({
+      'percent_amount': garapinCost,
+      'currency': 'IDR',
+      'destination_account_id': '6602d442a5e108f758e0651d',
+      'reference_id': 'garapin_pos'
+    });
+
+    console.log('total percent');
+    console.log(totalPercentAmount);
+    //check trx sudah terdaftar belum, karna satu trx satu split rule,
+    const referenceIdTRX = routes.filter(item => item.type === 'TRX').map(item => item.reference_id);
+    console.log(referenceIdTRX);
+    const splitPaymentRuleId = await connectTargetDatabase(referenceIdTRX);
+    const SplitPaymentRuleIdStore = splitPaymentRuleId.model('Split_Payment_Rule_Id', splitPaymentRuleIdScheme);
+    const existRuleTRX = await SplitPaymentRuleIdStore.findOne({ id_template: id_template });
+    if (!existRuleTRX) {
+      return apiResponse(res, 400, 'Sudah terdaftar ditemplate lain');
     }
-  };
+    const endpoint = 'https://api.xendit.co/split_rules';
+    const headers = {
+      'Authorization': `Basic ${Buffer.from(apiKey + ':').toString('base64')}`,
+    };
+    const response = await axios.post(endpoint, data, { headers });
+    if (response.status === 200) {
+      for (const route of response.data.routes) {
+        const splitPaymentRuleId = await connectTargetDatabase(route.reference_id);
+        const SplitPaymentRuleIdStore = splitPaymentRuleId.model('Split_Payment_Rule_Id', splitPaymentRuleIdScheme);
+
+        // search SplitRuleId sudah ada belum
+        const splitRuleId = await SplitPaymentRuleIdStore.findOne({ id_template: id_template });
+        if (!splitRuleId) {
+          const create = new SplitPaymentRuleIdStore({
+            id: response.data.id,
+            name: response.data.name,
+            description: response.data.description,
+            created_at: response.data.created_at,
+            updated_at: response.data.updated_at,
+            id_template: id_template, // Isi dengan nilai id_template yang sesuai
+            routes: response.data.routes
+          });
+          const data = await create.save();
+        } else {
+          const update = {
+            id: response.data.id,
+            name: response.data.name,
+            description: response.data.description,
+            created_at: response.data.created_at,
+            updated_at: response.data.updated_at,
+            routes: response.data.routes
+          };
+          const data = await SplitPaymentRuleIdStore.findOneAndUpdate(
+            { id_template: id_template },
+            update,
+            { new: true }
+          );
+        }
+        // end search SplitRuleId sudah ada belum
+      }
+
+      console.log(response.data);
+
+      return apiResponse(res, 200, 'Sukses membuat split rule', response.data);
+    }
+    return apiResponse(res, 400, 'gagal');
+
+
+  } catch (error) {
+    console.error('Error:', error.response?.data || error.message);
+    return apiResponse(res, 400, 'error');
+  }
+};
 
 
 const createTemplate = async (req, res) => {
@@ -146,7 +146,7 @@ const createTemplate = async (req, res) => {
     if (routes.length > 4) {
       return apiResponse(res, 200, 'Target bagi-bagi maksimal 4');
     }
-    const create = new Template({ name:name, description:description, routes:routes });
+    const create = new Template({ name: name, description: description, routes: routes });
     await create.save();
 
     return apiResponse(res, 200, 'Sukses membuat template', create);
@@ -168,15 +168,15 @@ const addFeeCustomer = async (req, res) => {
     const totalPercentFeePos = existingTemplate.routes.reduce((acc, route) => acc + route.fee_pos, 0);
     let updatedTemplate;
     if (totalPercentFeePos + percent_fee_cust !== 100) {
-       updatedTemplate = await Template.findOneAndUpdate(
-        { '_id': id_template }, 
-        { $set: { status_template: 'INACTIVE', fee_cust: percent_fee_cust } }, 
+      updatedTemplate = await Template.findOneAndUpdate(
+        { '_id': id_template },
+        { $set: { status_template: 'INACTIVE', fee_cust: percent_fee_cust } },
         { new: true }
       );
     } else {
       updatedTemplate = await Template.findOneAndUpdate(
-        { '_id': id_template }, 
-        { $set: { status_template: 'ACTIVE', fee_cust: percent_fee_cust } }, 
+        { '_id': id_template },
+        { $set: { status_template: 'ACTIVE', fee_cust: percent_fee_cust } },
         { new: true }
       );
     }
@@ -206,7 +206,7 @@ const deleteTargetTemplate = async (req, res) => {
     }
     const db = await connectTargetDatabase(targetDatabase);
     const Template = db.model('Template', templateSchema);
-    const { id, reference_id } = req.body; 
+    const { id, reference_id } = req.body;
     if (!id) {
       return apiResponse(res, 400, 'Document ID is required');
     }
@@ -231,11 +231,11 @@ const updateTemplate = async (req, res) => {
     if (!targetDatabase) {
       return apiResponse(res, 400, 'Target database is not specified');
     }
-    
+
     const db = await connectTargetDatabase(targetDatabase);
     const Template = db.model('Template', templateSchema);
-    
-    const { id, reference_id, route } = req.body; 
+
+    const { id, reference_id, route } = req.body;
 
 
     if (!id) {
@@ -250,54 +250,56 @@ const updateTemplate = async (req, res) => {
     const existingRoute = existingTemplate.routes.find(r => r.reference_id === reference_id);
     if (existingRoute) {
       const updatedTemplate = await Template.findOneAndUpdate(
-        { '_id': id, 'routes.reference_id': reference_id }, 
-        { $set: { 'routes.$': route, status_template:'INACTIVE' } }, 
+        { '_id': id, 'routes.reference_id': reference_id },
+        { $set: { 'routes.$': route, status_template: 'INACTIVE' } },
         { new: true }
       );
       return apiResponse(res, 200, 'Template updated', updatedTemplate);
     } else {
-    if (route.type === 'TRX') {
-      if (existingTemplate.db_trx === route.reference_id) {
-        console.log('sama nih');
-        const updatedTemplate = await Template.findOneAndUpdate(
-          { '_id': id, 'routes.type': 'TRX', status_template:'INACTIVE' }, 
-          { $set: { 'routes.$': route, status_template:'INACTIVE', db_trx: route.reference_id } }, 
-          { new: true }
-        );
-        console.log('sama nih3');
-        return apiResponse(res, 200, 'Template updated', updatedTemplate);
-      } else {
-        console.log('sama nih 5');
-        const dbTemplate = await connectTargetDatabase(targetDatabase);
-        const Template = dbTemplate.model('Template', templateSchema);
-        const template = await Template.find();
-        const foundTemplate = template.find(temp => temp.db_trx && temp.db_trx.includes(route.reference_id));
-        if (!foundTemplate) {
+      if (route.type === 'TRX') {
+        if (existingTemplate.db_trx === route.reference_id) {
+          console.log('sama nih');
           const updatedTemplate = await Template.findOneAndUpdate(
-            { '_id': id, 'routes.type': 'TRX', status_template:'INACTIVE' }, 
-            { $set: { 'routes.$': route, status_template:'INACTIVE', db_trx: route.reference_id } }, 
+            { '_id': id, 'routes.type': 'TRX', status_template: 'INACTIVE' },
+            { $set: { 'routes.$': route, status_template: 'INACTIVE', db_trx: route.reference_id } },
             { new: true }
           );
+          console.log('sama nih3');
           return apiResponse(res, 200, 'Template updated', updatedTemplate);
+        } else {
+          console.log('sama nih 5');
+          const dbTemplate = await connectTargetDatabase(targetDatabase);
+          const Template = dbTemplate.model('Template', templateSchema);
+          const template = await Template.find();
+          const foundTemplate = template.find(temp => temp.db_trx && temp.db_trx.includes(route.reference_id));
+          if (!foundTemplate) {
+            const updatedTemplate = await Template.findOneAndUpdate(
+              { '_id': id, 'routes.type': 'TRX', status_template: 'INACTIVE' },
+              { $set: { 'routes.$': route, status_template: 'INACTIVE', db_trx: route.reference_id } },
+              { new: true }
+            );
+            return apiResponse(res, 200, 'Template updated', updatedTemplate);
+          } else {
+            return apiResponse(res, 400, 'trx sudah digunakan ditemplate lain');
+          }
+
+        }
       } else {
-        return apiResponse(res, 400, 'trx sudah digunakan ditemplate lain');
+        console.log(existingTemplate.routes.length);
+        if (existingTemplate.routes.length > 3) {
+          return apiResponse(res, 400, 'Target bagi-bagi maksimal 4');
+        }
+        const updatedTemplate = await Template.findOneAndUpdate(
+          { '_id': id },
+          {
+            $push: { routes: route },
+            $set: { status_template: 'INACTIVE' }
+          }, // Memasukkan rute baru ke dalam array routes
+          { new: true }
+        );
+        return apiResponse(res, 200, 'New data inserted into routes', updatedTemplate);
       }
-      
-      }
-    } else {
-      console.log(existingTemplate.routes.length);
-      if (existingTemplate.routes.length > 3) {
-        return apiResponse(res, 400, 'Target bagi-bagi maksimal 4');
-      }
- const updatedTemplate = await Template.findOneAndUpdate(
-  { '_id': id },
-  { $push: { routes: route }, 
-  $set: { status_template: 'INACTIVE' } }, // Memasukkan rute baru ke dalam array routes
-  { new: true }
-);
-return apiResponse(res, 200, 'New data inserted into routes', updatedTemplate);
-    }
-     
+
     }
   } catch (error) {
     console.error('Error:', error.response?.data || error.message);
@@ -313,29 +315,29 @@ const activationTemplate = async (req, res) => {
     }
     const db = await connectTargetDatabase(targetDatabase);
     const Template = db.model('Template', templateSchema);
-    const { id_template, status } = req.body; 
+    const { id_template, status } = req.body;
     if (!id_template) {
       return apiResponse(res, 400, 'Document ID is required');
     }
     const existingTemplate = await Template.findById(id_template);
     const totalPercentAmount = existingTemplate.routes.reduce((acc, route) => acc + route.percent_amount, 0);
     if (totalPercentAmount !== 100) {
-       return apiResponse(res, 400, 'total persen bagi-bagi pendapatan harus 100%');
+      return apiResponse(res, 400, 'total persen bagi-bagi pendapatan harus 100%');
     }
     const totalPercentFeePos = existingTemplate.routes.reduce((acc, route) => acc + route.fee_pos, 0);
     if (totalPercentFeePos + existingTemplate.fee_cust !== 100) {
-       return apiResponse(res, 400, 'total persen bagi-bagi biaya harus 100%');
+      return apiResponse(res, 400, 'total persen bagi-bagi biaya harus 100%');
     }
     if (!existingTemplate) {
       return apiResponse(res, 404, 'Template not found');
     }
-      const updatedTemplate = await Template.findOneAndUpdate(
-        { '_id': id_template }, 
-        { $set: { status_template: status } }, 
-        { new: true }
-      );
-    
-      return apiResponse(res, 200, `Template ${status}`, updatedTemplate);
+    const updatedTemplate = await Template.findOneAndUpdate(
+      { '_id': id_template },
+      { $set: { status_template: status } },
+      { new: true }
+    );
+
+    return apiResponse(res, 200, `Template ${status}`, updatedTemplate);
   } catch (error) {
     console.error('Error:', error.response?.data || error.message);
     return apiResponse(res, 400, 'Error');
@@ -382,7 +384,7 @@ const getAllTemplates = async (req, res) => {
     return apiResponseList(res, 200, 'Success', template);
   } catch (error) {
     console.error('Error:', error.response?.data || error.message);
-    return apiResponseList(res, 400,  error.message);
+    return apiResponseList(res, 400, error.message);
   }
 };
 
