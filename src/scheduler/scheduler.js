1ddc7526d1074e55389720bc3de896b8edd400fc
Rifando Panggabean <fandofast@gmail.com>
2024-08-21 16:23:51 +0700

    fix error

--- src/scheduler/scheduler.js
+++ src/scheduler/scheduler.js
@@ -11,11 +11,11 @@ function setupCronJobs() {
   // "0 0 * * *" midnight
   // "*/5 * * * *"; every 5 menit
   // "0 * * * *" every hours
-  const scheduleUpdateAndCheckRack = "0 0 * * *";
-  cron.schedule(scheduleUpdateAndCheckRack, () => {
-    Logger.log("Running cron job for check Available Rak");
-    anotherEngine.schedulerStatusRakEngine();
-  });
+  // const scheduleUpdateAndCheckRack = "0 0 * * *";
+  // cron.schedule(scheduleUpdateAndCheckRack, () => {
+  //   Logger.log("Running cron job for check Available Rak");
+  //   anotherEngine.schedulerStatusRakEngine();
+  // });
 }
 
 export default setupCronJobs;
