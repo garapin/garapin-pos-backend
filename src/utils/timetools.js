84669b008c0e6381c50fe70d414003e866340539
Rifando Panggabean <fandofast@gmail.com>
2024-08-22 22:57:55 +0700

    fix INCOMING

--- src/utils/timetools.js
+++ src/utils/timetools.js
@@ -17,7 +17,7 @@ const isIncoming = (endDate,due_date ) => {
   const now = moment().tz('GMT');
   const end_date = moment(endDate).tz('GMT');
   const daysUntilDue = end_date.diff(now, 'days');
-  const isDueDate = (daysUntilDue <= due_date) && (now.isBefore(end_date));  
+  const isDueDate = (daysUntilDue < due_date) && (now.isBefore(end_date));  
   console.log(end_date);
   console.log(daysUntilDue);
   
